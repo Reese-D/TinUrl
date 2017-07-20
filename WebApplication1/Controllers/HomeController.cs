400e5effdadd4af17d5bc264f2699559bd53107e
Reese De Wind <reese.dewind@gmail.com>
2017-07-19 20:56:46 -0400

    registered the dependency resolver in startup and fixed some csproj/ sln issues created when one of the projects was renamed

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using Domain.Library.Repositories;
+using Domain.Library.Services;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +12,15 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        protected IUserService UserService { get; set; }
+        public HomeController(IUserService userService)
+        {
+            UserService = userService;
+        }
+
         public ActionResult Index(string redirectId)
         {
+            var user = UserService.LoadUser("3fcf7849-f773-4dc3-873d-180cfde32b95");
             return View();
         }
 
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,15 +1,37 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using System.Web.Mvc;
+using StructureMap;
+using Domain.Library.Repositories;
+using Domain.Implementation.Repositories;
+using DependencyInjection;
+using Domain.AppData;
+using Domain.Library.Services;
+using Domain.Implementation.Services;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
 namespace WebApplication1
 {
     public partial class Startup
     {
+
+        protected LinqToSqlDataDataContext DataContext { get; set; }
         public void Configuration(IAppBuilder app)
         {
+            DataContext = new LinqToSqlDataDataContext();
+            InitializeContainer();
             ConfigureAuth(app);
+        }
+
+        public void InitializeContainer()
+        {
+            var container = new Container(x =>
+            {
+                x.For<IUserRepository>().Use<UserRepository>();
+                x.For<IUserService>().Use<UserService>();
+                x.For<LinqToSqlDataDataContext>().Use(DataContext);
+            });
+            DependencyResolver.SetResolver(new TinyUrlDependencyResolver(container));
         }
     }
 }
