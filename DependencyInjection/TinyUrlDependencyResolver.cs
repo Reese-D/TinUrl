c9d936e710249b5061dfd2c69b7f1c5af75e0307
Reese De Wind <reese.dewind@gmail.com>
2017-07-19 20:54:24 -0400

    ignored /bin and /obj files, removed the ones that were accidently pushed up in prior commits, and created a dependency injection project to handle inversion of control

---
+++ DependencyInjection/TinyUrlDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DependencyInjection
+{
+    public class TinyUrlDependencyResolver : IDependencyResolver
+    {
+        protected readonly IContainer Container;
+
+        public TinyUrlDependencyResolver(IContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+
+            Container = container;
+        }
+
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == null)
+                return null;
+
+            return (serviceType.IsAbstract || serviceType.IsInterface)
+                        ? Container.TryGetInstance(serviceType)
+                        : Container.GetInstance(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Container.GetAllInstances(serviceType)
+                .Cast<object>()
+                .Where(s => s.GetType() == serviceType);
+        }
+    }
+}
