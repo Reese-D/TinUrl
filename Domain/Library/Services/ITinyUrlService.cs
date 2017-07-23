be364177c7bfd0d31cd0e42668030747a5395553
Reese De Wind <reese.dewind@gmail.com>
2017-07-23 18:05:08 -0400

    added the moved url service file

---
+++ Domain/Library/Services/ITinyUrlService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Domain.Library.Services
+{
+    public interface ITinyUrlService
+    {
+        List<TinyUrl> LoadAllUrls();
+        List<TinyUrl> LoadUrlsForUser(string userID);
+        TinyUrl CreateUrl(string url, string userID);
+        TinyUrl LoadUrl(string urlID);
+    }
+}
